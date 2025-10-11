cb7919acf0f3b2b903af1cf2594a6c43a6085db2
shreyaupd <shreyaupd234@gmail.com>
2025-10-11 22:49:13 +0545

    Enhance About page with GSAP animations and integrate country data

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRef } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger);
+import countryfacts from '../api/countryData.json'
+const About = ({animate}) => {
+  const cardref=useRef([]);
+  useEffect(() => {
+    if(!animate) return;
+    const ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        delay: 0.5
+      });
+      tl.fromTo(cardref.current,
+        { y: 100, opacity: 0 },
+        { y: 0, 
+          opacity: 1, 
+          stagger: 0.2, 
+          duration: 1, 
+          ease: "power2.out",
+          scrollTrigger:{
+            trigger:".card",
+            start:"top 80%",
+            end :"bottom 20%",
+            scrub:true,
+            toggleActions:"play none none reverse",
+          }
+        }
+      )
 
-const About = () => {
+  })
+  return ()=>ctx.revert();
+},[animate])
+
+  const countryref= (el)=>{
+    if(el && !cardref.current?.includes(el)){
+      cardref.current.push(el)
+    }
+  }
   return (
-    <div>About</div>
+    <section className='section-about container'>
+      <h2 className='container-title'>
+        Here are the interesting facts <br /> we are proud of
+      </h2>
+      <div className='gradient-cards'>
+        {countryfacts.map((country)=>{
+          const {id,countryName,capital,population,interestingFact}=country;
+          return (
+           <div ref={countryref} className='card' key={id}>
+          <div className='container-card bg-blue-box'>
+            <p className='card-title'> {countryName}  </p>
+            <p>
+              <span className='card-description'>
+                Capital:
+              </span>
+              {capital}
+            </p>
+            <p>
+              <span className='card-description'>Population: </span>
+              {population}
+            </p>
+            <p>
+              <span className="card-description">Interesting Facts:</span>
+              {interestingFact}
+            </p>
+          </div>
+        </div>
+          )
+        })}
+        
+      </div>
+    </section>
+
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
-import { useRef, useEffect } from 'react'
+import { useState,useRef, useEffect } from 'react'
 import { gsap } from 'gsap'
 import world from '../assets/image/world.png'
 import { FaArrowRight } from "react-icons/fa";
-
+import About from './About';
 const Home = () => {
   const headingref = useRef(null)
   const imgref = useRef(null)
+  const [homeAnimation, sethomeAnimation] = useState(false)
  
   useEffect(() => {
     const ctx = gsap.context(() => {
-      const tl = gsap.timeline({ delay: 0.5 });
+      const tl = gsap.timeline({ 
+        delay: 0.5, 
+        onComplete:()=>sethomeAnimation(true)
+      });
       tl.from(headingref.current, {
         opacity: 0,
         x: -50,
@@ -28,7 +32,8 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="mt-30 md:mt-0 flex flex-col items-center justify-center h-[70vh] gap-10 text-3xl font-display">
+  <div>
+      <div className="mt-30 md:mt-0 flex flex-col items-center justify-center h-[70vh] gap-10 text-3xl font-display">
       <div className="flex flex-col lg:flex-row justify-between items-center w-full px-4">
         <div ref={headingref} className="lg:ml-[90px] w-full lg:w-[500px] h-auto lg:h-[40vh]">
           <h1 className="heading-xl">
@@ -47,6 +52,12 @@ const Home = () => {
         </div>
       </div>
     </div>
+     <div className='flex justify-center items-center mt-40 w-9/10 mx-auto rounded-lg py-10'>
+       <About animate={homeAnimation}/>
+     </div>
+  </div>
+
+    
   )
 }
 
