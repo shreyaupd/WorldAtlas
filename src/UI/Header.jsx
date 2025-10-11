1ec3901174b451df428807d16a57d3165346243b
shreyaupd <shreyaupd234@gmail.com>
2025-10-11 13:49:20 +0545

    Refactor code structure for improved readability and maintainability

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex justify-center items-center bg-[#1c1c1c] h-30  w-full '>
+      <div className='flex justify-between items-center w-8/10 p-5 h-30'>
+        <div className="text-4xl font-display md:text-5xl text-white font-logo tracking-wider drop-shadow-lg">
+          WORLDATLAS
+        </div>
+        <div className='text-white text-xl'>
+          <ul className='cursor-pointer flex gap-15'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Country</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { useRef, useEffect, use } from 'react'
+import { gsap } from 'gsap'
+import world from '../assets/image/world.png'
 
 const Home = () => {
+  const headingref = useRef(null)
+  const imgref = useRef(null)
+ 
+useEffect(() => {
+  const ctx = gsap.context(() => {
+    const tl = gsap.timeline({ delay: 0.5 });
+    tl.from(headingref.current, {
+      opacity: 0,
+      x: -50,
+      duration: 1,
+    }).from(
+      imgref.current,
+      {
+        opacity: 0,
+        x: 50,
+        duration: 1,
+      },
+      "-=0.5"
+    );
+  },
+  );
+  return () => ctx.revert();   
+},[])
+
+
   return (
-    <div>Home</div>
+    <div className="flex flex-col items-center justify-center h-[70vh] gap-10 text-3xl font-display">
+      <div className="flex justify-between">
+        <div ref={headingref} className="ml-[90px] w-[850px] h-[40vh]">
+          <h1 className="heading-xl">
+            Explore the world<br /> one country at a time.
+          </h1>
+          <p className="paragraph">
+            Discover the history, culture and beauty of every corner of the globe.
+            Sort, search and filter through countries to find the details you need.
+          </p>
+        </div>
+        <div ref={imgref}>
+          <img className="ml-40 hero-image w-8/10" src={world} alt="World" />
+        </div>
+      </div>
+    </div>
   )
 }
 
